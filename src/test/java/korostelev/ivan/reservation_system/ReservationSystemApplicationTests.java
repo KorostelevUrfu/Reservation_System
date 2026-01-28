eba055b3202bd03986425e8735be9fd8ea404895
Korostelev Ivan <ikorosteleff@gmail.com>
2026-01-28 23:32:07 +0500

    init commit

---
+++ src/test/java/korostelev/ivan/reservation_system/ReservationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package korostelev.ivan.reservation_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
